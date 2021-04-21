eb08c96f6cc37bb66246ae5f16f02b9ec89571c7
usu2 name <usu2exmaple.com>
2021-04-21 18:39:16 +0200

    Qué ocurre
    Lo que ocurre al fusionar la rama "Cambios" con la rama "Master" es que
    las dos clases que antes creé en "Cambio" ahora pertenecen a "Master"

---
+++ src/Cambio1.java
@@ -0,0 +1,5 @@
+public class Cambio1 {
+	public static void main(String[] args) {
+		System.out.println("Cambio 1");
+	}
+}
---
+++ src/Cambio2.java
@@ -0,0 +1,5 @@
+public class Cambio2 {
+	public static void main(String[] args) {
+		System.out.println("Cambio 2");
+	}
+}
